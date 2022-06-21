079c93bf807f835c1dd4aed2666e43ab4973b1a6
justin6302971 <justin6302971@hotmail.com>
2022-06-21 23:10:21 +0800

    setup Mediator for the project with test endpoints

--- src/CleanArchitecture.WebApi/Controllers/WeatherForecastController.cs
+++ src/CleanArchitecture.WebApi/Controllers/WeatherForecastController.cs
@@ -2,38 +2,19 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using CleanArchitecture.Application.WeatherForecasts.Queries.GetWeatherForecasts;
+using CleanArchitecture.Domain.DTO;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Logging;
 
 namespace CleanArchitecture.WebApi.Controllers
 {
-    [ApiController]
-    [Route("[controller]")]
-    public class WeatherForecastController : ControllerBase
+    public class WeatherForecastController : ApiController
     {
-        private static readonly string[] Summaries = new[]
-        {
-            "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
-        };
-
-        private readonly ILogger<WeatherForecastController> _logger;
-
-        public WeatherForecastController(ILogger<WeatherForecastController> logger)
-        {
-            _logger = logger;
-        }
-
         [HttpGet]
-        public IEnumerable<WeatherForecast> Get()
+        public async Task<IEnumerable<WeatherForecast>> Get()
         {
-            var rng = new Random();
-            return Enumerable.Range(1, 5).Select(index => new WeatherForecast
-            {
-                Date = DateTime.Now.AddDays(index),
-                TemperatureC = rng.Next(-20, 55),
-                Summary = Summaries[rng.Next(Summaries.Length)]
-            })
-            .ToArray();
+            return await Mediator.Send(new GetWeatherForecastsQuery());
         }
     }
 }
--- src/CleanArchitecture.WebApi/Startup.cs
+++ src/CleanArchitecture.WebApi/Startup.cs
@@ -2,6 +2,7 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using CleanArchitecture.Application;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.HttpsPolicy;
@@ -25,6 +26,11 @@ namespace CleanArchitecture.WebApi
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddApplication();
+            // services.AddInfrastructure(Configuration);
+
+            services.AddHttpContextAccessor();
+
             services.AddControllers();
         }
 
